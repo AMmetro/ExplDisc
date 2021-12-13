c09cd750f01f961393ee351cd31b35552257d68d
Andrei <7656077@mail.ru>
2021-12-13 13:31:27 +0300

    Revert "try delete some lines"
    This reverts commit 1771abee2e6db5cd741a06b3af326df4eb1bc794.

--- test.jsx
+++ test.jsx
@@ -1,7 +1,11 @@
 console.log('line 1')
-
+console.log('new line 1111')
 console.log('line 2')
-
+console.log('new line 2222')
 console.log('line 3')
-
+console.log('new line 3333')
 console.log('line 4')
+
+console.log('line 5555 666 777')
+
+console.log('line 99999 888 000')
