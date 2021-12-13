2fefcf7cf57ac110fc2538ed5f9b37c4c41f7ecb
Andrei <7656077@mail.ru>
2021-12-13 15:41:32 +0300

    clear

--- src/lib/juiceplus/cms/index.ts
+++
@@ -1,8 +0,0 @@
-import {aemHost} from '../../config'
-import type {Locale} from '../../i18n/locale'
-import {AemClient} from '../atd/aem/aem-client'
-import {getCms as getAtdCms} from '../atd/cms'
-
-export const getClient = (locale?: Locale) => new AemClient(aemHost, locale)
-
-export const getCms = (locale?: Locale) => getAtdCms(getClient(locale))
