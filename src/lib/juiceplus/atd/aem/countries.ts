ec76a792380472d8d7b4ccf7aae381bcd9658670
Andrei <7656077@mail.ru>
2021-12-13 16:05:35 +0300

    clear

--- src/lib/juiceplus/atd/aem/countries.ts
+++
@@ -1,9 +0,0 @@
-import type {CmsClient} from '../../cms/client'
-
-export type CountriesMap = {
-  [key: string]: string
-}
-
-export const countries = async (client: CmsClient): Promise<CountriesMap> => {
-  return await client.request('_jcr_content.countries.json')
-}
