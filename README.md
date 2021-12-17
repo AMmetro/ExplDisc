15345116734060214386f623eb47d04ca062061d
home <7656077@mail.ru>
2021-12-18 00:56:28 +0300

    clean folder

--- README.md
+++
@@ -1,1 +0,0 @@
-add mock data for short trk
--- e2e/setupTests.ts
+++
@@ -1,4 +0,0 @@
-jest.setTimeout(60000)
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function,import/no-anonymous-default-export
-export default () => {}
