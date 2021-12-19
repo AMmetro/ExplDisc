420cf8e0e38b435eed95701fa117293a18b5d6dd
home <7656077@mail.ru>
2021-12-19 11:06:13 +0300

    clean

--- src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-export function Spinner() {
-  return (
-    <div
-      style={{borderTopColor: 'transparent'}}
-      className="w-6 h-6 border-2 border-orange rounded-full loader animate-spin"
-    />
-  )
-}
--- src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import type {PropsWithChildren} from 'react'
-
-export function Card({children}: PropsWithChildren<unknown>) {
-  return (
-    <div className="bg-white shadow-menu overflow-hidden rounded-2xl">
-      {children}
-    </div>
-  )
-}
